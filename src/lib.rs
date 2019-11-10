3051e6cee4dde9cf02f18cf716e9842b15be4273
Alex Constantinou <a_constanttinou@hotmail.co.uk>
2019-11-10 00:16:40 +0000

    cpu struct created

--- src/lib.rs
+++ src/lib.rs
@@ -3,8 +3,44 @@ use std::path::Path;
 use std::str;
 use std::io;
 
+pub struct CPUUsage {
+    pub usage: Vec<f32>,
+    pub time: Vec<f32>,
+}
 
-pub fn get_cpu_info() -> io::Result<()> {
+
+impl CPUUsage {
+
+    pub fn new() -> CPUUsage {
+        let usage = vec![];
+        let time = vec![];
+        CPUUsage {
+            usage,
+            time
+        }
+    }
+
+    pub fn add_cpu_data(&mut self) -> io::Result<()>{
+        let new_cpu_data = get_cpu_info()?;
+        self.usage.push(new_cpu_data);
+        let time_to_add = match self.time.last() {
+             Some(x) =>  x + 1.0,
+             None => 1.0
+        };
+        self.time.push(time_to_add);
+        Ok(())
+    }
+
+    pub fn get_usage(&self) -> &Vec<f32> {
+        self.usage.as_ref()
+    }
+    
+    pub fn get_time(&self) -> &Vec<f32> {
+        self.time.as_ref()
+    }
+}
+
+pub fn get_cpu_info() -> Result<f32, io::Error> {
     let cpu_file_path = Path::new("/proc/stat");
     let cpu_file = fs::read_to_string(cpu_file_path)?;
     let cpu_info = match cpu_file.lines().next() {
@@ -20,10 +56,8 @@ pub fn get_cpu_info() -> io::Result<()> {
         .map(| x | x.parse().unwrap())
         .collect();
 
-    println!("{:?}", cpu_values);
     let cpu_usage = calculate_cpu_usage(&cpu_values);
-    println!("cpu {}%", cpu_usage);
-    Ok(())
+    Ok(cpu_usage)
 }
 
 fn calculate_cpu_usage(cpu_vec: &Vec<f32>) -> f32 {
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,17 @@
-use system_info;
+use system_info::CPUUsage;
 use std::process;
+use std::{thread, time};
 
 fn main() {
-    if let Err(e) = system_info::get_cpu_info() {
-        eprintln!("Application error: {}", e);
-        process::exit(1);
+    let mut cpu_usage = CPUUsage::new();
+    let second = time::Duration::from_millis(1000);
+    loop {
+        if let Err(e) = cpu_usage.add_cpu_data() {
+            eprintln!("Application error: {}", e);
+            process::exit(1);
+        }
+        println!("usage: {:?}", cpu_usage.get_usage());
+        println!("time: {:?}", cpu_usage.get_time());
+        thread::sleep(second);
     }
 }
