2a9084701bced580ea6f9d4c7a29802262a0159f
Alex Constantinou <a_constanttinou@hotmail.co.uk>
2019-11-10 01:17:12 +0000

    graph started

--- src/lib.rs
+++ src/lib.rs
@@ -4,8 +4,7 @@ use std::str;
 use std::io;
 
 pub struct CPUUsage {
-    pub usage: Vec<f32>,
-    pub time: Vec<f32>,
+    pub usage: Vec<(f64, f64)>,
 }
 
 
@@ -13,34 +12,27 @@ impl CPUUsage {
 
     pub fn new() -> CPUUsage {
         let usage = vec![];
-        let time = vec![];
         CPUUsage {
             usage,
-            time
         }
     }
 
     pub fn add_cpu_data(&mut self) -> io::Result<()>{
         let new_cpu_data = get_cpu_info()?;
-        self.usage.push(new_cpu_data);
-        let time_to_add = match self.time.last() {
-             Some(x) =>  x + 1.0,
+        let time_to_add = match self.usage.last() {
+             Some(x) =>  x.1 + 1.0,
              None => 1.0
         };
-        self.time.push(time_to_add);
+        self.usage.push((time_to_add, new_cpu_data));
         Ok(())
     }
 
-    pub fn get_usage(&self) -> &Vec<f32> {
+    pub fn get_usage(&self) -> &Vec<(f64, f64)> {
         self.usage.as_ref()
-    }
-    
-    pub fn get_time(&self) -> &Vec<f32> {
-        self.time.as_ref()
     }
 }
 
-pub fn get_cpu_info() -> Result<f32, io::Error> {
+pub fn get_cpu_info() -> Result<f64, io::Error> {
     let cpu_file_path = Path::new("/proc/stat");
     let cpu_file = fs::read_to_string(cpu_file_path)?;
     let cpu_info = match cpu_file.lines().next() {
@@ -51,7 +43,7 @@ pub fn get_cpu_info() -> Result<f32, io::Error> {
         .split(' ')
         .collect();
 
-    let cpu_values: Vec<f32> = cpu_vec
+    let cpu_values: Vec<f64> = cpu_vec
         .drain(2..)
         .map(| x | x.parse().unwrap())
         .collect();
@@ -60,7 +52,7 @@ pub fn get_cpu_info() -> Result<f32, io::Error> {
     Ok(cpu_usage)
 }
 
-fn calculate_cpu_usage(cpu_vec: &Vec<f32>) -> f32 {
+fn calculate_cpu_usage(cpu_vec: &Vec<f64>) -> f64 {
     let result = (cpu_vec[0] + cpu_vec[2]) * 100.0 / 
         (cpu_vec[0] + cpu_vec[2] + cpu_vec[3]);
     result
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,67 @@
 use system_info::CPUUsage;
 use std::process;
+use std::io;
 use std::{thread, time};
+use termion::event::Key;
+use termion::input::MouseTerminal;
+use termion::raw::IntoRawMode;
+use termion::screen::AlternateScreen;
+use tui::backend::TermionBackend;
+use tui::style::{Color, Modifier, Style};
+use tui::widgets::{Axis, Block, Borders, Chart, Dataset, Marker, Widget};
+use tui::Terminal;
 
-fn main() {
+
+fn main() -> Result<(), io::Error> {
     let mut cpu_usage = CPUUsage::new();
     let second = time::Duration::from_millis(1000);
+    let stdout = io::stdout().into_raw_mode()?;
+    let stdout = MouseTerminal::from(stdout);
+    let stdout = AlternateScreen::from(stdout);
+    let backend = TermionBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
+    terminal.hide_cursor()?;
     loop {
         if let Err(e) = cpu_usage.add_cpu_data() {
             eprintln!("Application error: {}", e);
             process::exit(1);
         }
-        println!("usage: {:?}", cpu_usage.get_usage());
-        println!("time: {:?}", cpu_usage.get_time());
+     terminal.draw(|mut f| {
+        let size = f.size();
+        Chart::default()
+        .block(
+            Block::default()
+                    .title("Chart")
+                    .title_style(Style::default().fg(Color::Cyan).modifier(Modifier::BOLD))
+                    .borders(Borders::ALL),
+            )
+            .x_axis(
+                Axis::default()
+                    .title("X Axis")
+                    .style(Style::default().fg(Color::Gray))
+                    .labels_style(Style::default().modifier(Modifier::ITALIC))
+                    .bounds([0.0, 100.0])
+                    .labels(&["0", "25", "50", "75", "100"]),
+            )
+            .y_axis(
+                Axis::default()
+                    .title("Y Axis")
+                    .style(Style::default().fg(Color::Gray))
+                    .labels_style(Style::default().modifier(Modifier::ITALIC))
+                    .bounds([0.0, 10.0])
+                    .labels(&["0", "2", "4", "6", "8", "10"]),
+            )
+            .datasets(&[
+                Dataset::default()
+                    .name("data2")
+                    .marker(Marker::Dot)
+                    .style(Style::default().fg(Color::Cyan))
+                    .data(&cpu_usage.get_usage()[..]),
+            ])
+            .render(&mut f, size);
+        })?;
+        //println!("usage: {:?}", cpu_usage.get_usage();
         thread::sleep(second);
-    }
+        }
+    Ok(())
 }
