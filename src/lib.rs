26b8c9bb33de45d39c9fc1928c660f3d409a1280
Alex Constantinou <a_constanttinou@hotmail.co.uk>
2019-11-19 01:14:23 +0000

    code formatted

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,8 @@
-use std::{ fs };
+use regex::Regex;
+use std::fs;
+use std::io;
 use std::path::Path;
 use std::str;
-use std::io;
-use regex::Regex;
 
 #[derive(Debug)]
 pub struct CPUUsage {
@@ -12,9 +12,7 @@ pub struct CPUUsage {
     current_cpu: f64,
 }
 
-
 impl CPUUsage {
-
     pub fn new() -> CPUUsage {
         let usage = vec![];
         let prev_time = 0.0;
@@ -35,18 +33,17 @@ impl CPUUsage {
     pub fn clear_usage(&mut self) {
         if self.usage.len() >= 300 {
             self.usage.clear();
-        } 
+        }
     }
 
-    pub fn add_cpu_data(&mut self) -> io::Result<()>{
+    pub fn add_cpu_data(&mut self) -> io::Result<()> {
         self.clear_usage();
         let new_cpu_data = self.get_cpu_info()?;
         let time_to_add = match self.usage.last() {
-             Some(x) =>  x.0 + 1.0,
-             None => 1.0
+            Some(x) => x.0 + 1.0,
+            None => 1.0,
         };
         self.usage.push((time_to_add, new_cpu_data));
-        // println!("{:?}", self.usage);
         Ok(())
     }
 
@@ -61,14 +58,9 @@ impl CPUUsage {
             Some(i) => i,
             None => "File Error",
         };
-        let mut cpu_vec: Vec<&str> = cpu_info
-            .split(' ')
-            .collect();
+        let mut cpu_vec: Vec<&str> = cpu_info.split(' ').collect();
 
-        let cpu_values: Vec<f64> = cpu_vec
-            .drain(2..)
-            .map(| x | x.parse().unwrap())
-            .collect();
+        let cpu_values: Vec<f64> = cpu_vec.drain(2..).map(|x| x.parse().unwrap()).collect();
 
         let cpu_usage = self.calculate_current_cpu_usage(&cpu_values);
         Ok(cpu_usage)
@@ -81,8 +73,8 @@ impl CPUUsage {
             self.prev_usage = current_usage;
             self.prev_time = current_time;
         }
-        let current_cpu = (current_usage - self.prev_usage) * 100.0 / 
-            (current_time - self.prev_time);
+        let current_cpu =
+            (current_usage - self.prev_usage) * 100.0 / (current_time - self.prev_time);
         self.prev_usage = current_usage;
         self.prev_time = current_time;
         self.current_cpu = current_cpu;
@@ -97,20 +89,16 @@ pub struct MemInfo {
 }
 
 impl MemInfo {
-
     pub fn new() -> MemInfo {
         let usage = vec![];
         let current_mem = 0.0;
-        MemInfo {
-            usage,
-            current_mem,
-        }
+        MemInfo { usage, current_mem }
     }
 
     pub fn clear_usage(&mut self) {
         if self.usage.len() >= 300 {
             self.usage.clear();
-        } 
+        }
     }
 
     pub fn get_usage(&self) -> &Vec<(f64, f64)> {
@@ -122,22 +110,20 @@ impl MemInfo {
     }
 
     pub fn extract_kb_info(&self, line: Vec<&str>) -> f64 {
-        let line_vec: Vec<&str> = line[0]
-            .split_whitespace()
-            .collect();
-        let kb: f64 = line_vec[1]
-            .parse()
-            .unwrap();
+        let line_vec: Vec<&str> = line[0].split_whitespace().collect();
+        let kb: f64 = line_vec[1].parse().unwrap();
         kb
     }
 
     pub fn get_mem_info(&mut self) -> Result<f64, io::Error> {
         let mem_file_path = Path::new("/proc/meminfo");
         let mem_file = fs::read_to_string(mem_file_path)?;
-        let mem_total_line: Vec<&str> = mem_file.lines()
+        let mem_total_line: Vec<&str> = mem_file
+            .lines()
             .filter(|line| line.contains("MemTotal"))
             .collect();
-       let mem_free_line: Vec<&str> = mem_file.lines()
+        let mem_free_line: Vec<&str> = mem_file
+            .lines()
             .filter(|line| line.contains("MemFree"))
             .collect();
         let mem_total = self.extract_kb_info(mem_total_line);
@@ -145,23 +131,18 @@ impl MemInfo {
         let percentage_used = mem_free / mem_total * 100.0;
         self.current_mem = percentage_used;
         Ok(percentage_used)
-
     }
 
-    pub fn add_mem_data(&mut self) -> io::Result<()>{
+    pub fn add_mem_data(&mut self) -> io::Result<()> {
         self.clear_usage();
         let new_mem_data = self.get_mem_info()?;
         let time_to_add = match self.usage.last() {
-             Some(x) =>  x.0 + 1.0,
-             None => 1.0
+            Some(x) => x.0 + 1.0,
+            None => 1.0,
         };
         self.usage.push((time_to_add, new_mem_data));
-        // println!("{:?}", self.usage);
         Ok(())
     }
-
-
-
 }
 
 #[derive(Debug, Clone)]
@@ -177,8 +158,7 @@ pub struct Process {
 }
 
 impl Process {
-
-    pub fn new(pid: i32, state: String,  process_name: String, utime: f64, stime: f64) -> Process {
+    pub fn new(pid: i32, state: String, process_name: String, utime: f64, stime: f64) -> Process {
         let total_time = utime + stime;
         let mem_percent = 0.0;
         let cpu_percent = 0.0;
@@ -201,14 +181,12 @@ impl Process {
 
 #[derive(Debug)]
 pub struct ProcessInfo {
-    processes: Vec<Process>, 
+    processes: Vec<Process>,
     cpu_time_diff: f64,
     current_cpu_time: f64,
 }
 
-
 impl ProcessInfo {
-    
     pub fn new() -> ProcessInfo {
         let processes = vec![];
         let cpu_time_diff = 0.0;
@@ -229,7 +207,7 @@ impl ProcessInfo {
             .sort_by(|a, b| b.cpu_percent.partial_cmp(&a.cpu_percent).unwrap());
     }
 
-    pub fn update(&mut self, proc_path: &Path) -> Result<(), io::Error>{
+    pub fn update(&mut self, proc_path: &Path) -> Result<(), io::Error> {
         self.update_cpu_diff()?;
         self.read_dirs(&proc_path)?;
         self.sort_by_cpu();
@@ -237,7 +215,6 @@ impl ProcessInfo {
     }
 
     pub fn read_dirs(&mut self, proc_path: &Path) -> Result<(), io::Error> {
-        // println!("{:?}", proc_path);
         let mut dirs = vec![];
         let mut path;
         let digits_only = Regex::new("^[0-9]*$").unwrap();
@@ -246,14 +223,13 @@ impl ProcessInfo {
             path = entry.path();
             if path.is_dir() && digits_only.is_match(path.file_name().unwrap().to_str().unwrap()) {
                 dirs.push(path);
-            }
-            else if path.is_file() && path.file_name().unwrap() == "stat" 
-                && proc_path.to_str().unwrap() != "/proc/" {
+            } else if path.is_file()
+                && path.file_name().unwrap() == "stat"
+                && proc_path.to_str().unwrap() != "/proc/"
+            {
                 let contents = match fs::read_to_string(&path) {
                     Ok(f) => f,
-                    Err(e) => {
-                        e.to_string()
-                    },
+                    Err(e) => e.to_string(),
                 };
                 self.get_mem_data(&contents);
             }
@@ -265,22 +241,14 @@ impl ProcessInfo {
     }
 
     pub fn get_mem_data(&mut self, contents: &str) -> Result<(), io::Error> {
-        let contents_array: Vec<&str> = contents
-            .split_whitespace()
-            .collect();
+        let contents_array: Vec<&str> = contents.split_whitespace().collect();
         let pid: i32 = contents_array[0].parse().unwrap();
         let process_name: String = contents_array[2].replace("(", "").replace(")", "");
         let state: String = contents_array[1].replace("(", "").replace(")", "");
         let utime: f64 = contents_array[13].parse().unwrap();
         let stime: f64 = contents_array[14].parse().unwrap();
         let total_time = utime + stime;
-        let mem_data = Process::new(
-            pid,
-            process_name,
-            state,
-            utime,
-            stime,
-        );
+        let mem_data = Process::new(pid, process_name, state, utime, stime);
         self.add_to_processes(mem_data);
         Ok(())
     }
@@ -289,38 +257,37 @@ impl ProcessInfo {
         let current_cpu_time = self.get_cpu_info()?;
         self.cpu_time_diff = current_cpu_time - self.current_cpu_time;
         self.current_cpu_time = current_cpu_time;
-        // println!("{}", self.cpu_time_diff);
         Ok(())
     }
 
-
     pub fn add_to_processes(&mut self, mut process: Process) -> Result<(), io::Error> {
-        let found_process = self.processes.iter()
-            .find(|p| p.pid == process.pid);
+        let found_process = self.processes.iter().find(|p| p.pid == process.pid);
         match found_process {
             Some(p) => {
                 let utime_percent = 100.0 * (process.utime - p.utime) / self.cpu_time_diff;
                 let stime_percent = 100.0 * (process.stime - p.stime) / self.cpu_time_diff;
                 let percent = 100.0 * (process.total_time - p.total_time) / self.cpu_time_diff;
                 process.set_cpu_percent(percent);
-                let filtered_processes: Vec<Process> = self.processes.iter().cloned()
-                    .filter(| x| x.pid != process.pid)
+                let filtered_processes: Vec<Process> = self
+                    .processes
+                    .iter()
+                    .cloned()
+                    .filter(|x| x.pid != process.pid)
                     .collect();
                 self.processes = filtered_processes;
-                // println!("{:?}", process);
                 self.processes.push(process);
-            },
+            }
             None => self.processes.push(process),
         };
         Ok(())
     }
 
-    pub fn get_cpu_info(&mut self) -> Result<f64, io::Error>{
+    pub fn get_cpu_info(&mut self) -> Result<f64, io::Error> {
         let mut cpu_time = 0.0;
         let cpu_vec = create_cpu_vector()?;
         for val in cpu_vec.iter() {
             cpu_time += val;
-        } 
+        }
         Ok(cpu_time)
     }
 }
@@ -331,14 +298,9 @@ pub fn create_cpu_vector() -> Result<Vec<f64>, io::Error> {
     let cpu_info = match cpu_file.lines().next() {
         Some(i) => i,
         None => "File Error",
-     };
-    let mut cpu_vec: Vec<&str> = cpu_info
-        .split(' ')
-        .collect();
+    };
+    let mut cpu_vec: Vec<&str> = cpu_info.split(' ').collect();
 
-    let cpu_values: Vec<f64> = cpu_vec
-        .drain(2..)
-        .map(| x | x.parse().unwrap())
-        .collect();
+    let cpu_values: Vec<f64> = cpu_vec.drain(2..).map(|x| x.parse().unwrap()).collect();
     Ok(cpu_values)
 }
