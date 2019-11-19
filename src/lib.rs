8a423ce61ad0ab25ea84136cec456a43d894095f
Alex Constantinou <a_constanttinou@hotmail.co.uk>
2019-11-19 22:37:41 +0000

    can show mem of process

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,8 @@
-use regex::Regex;
-use std::fs;
-use std::io;
+use std::{ fs };
 use std::path::Path;
 use std::str;
+use std::io;
+use regex::Regex;
 
 #[derive(Debug)]
 pub struct CPUUsage {
@@ -12,7 +12,9 @@ pub struct CPUUsage {
     current_cpu: f64,
 }
 
+
 impl CPUUsage {
+
     pub fn new() -> CPUUsage {
         let usage = vec![];
         let prev_time = 0.0;
@@ -33,15 +35,15 @@ impl CPUUsage {
     pub fn clear_usage(&mut self) {
         if self.usage.len() >= 300 {
             self.usage.clear();
-        }
+        } 
     }
 
-    pub fn add_cpu_data(&mut self) -> io::Result<()> {
+    pub fn add_cpu_data(&mut self) -> io::Result<()>{
         self.clear_usage();
         let new_cpu_data = self.get_cpu_info()?;
         let time_to_add = match self.usage.last() {
-            Some(x) => x.0 + 1.0,
-            None => 1.0,
+             Some(x) =>  x.0 + 1.0,
+             None => 1.0
         };
         self.usage.push((time_to_add, new_cpu_data));
         Ok(())
@@ -58,9 +60,14 @@ impl CPUUsage {
             Some(i) => i,
             None => "File Error",
         };
-        let mut cpu_vec: Vec<&str> = cpu_info.split(' ').collect();
+        let mut cpu_vec: Vec<&str> = cpu_info
+            .split(' ')
+            .collect();
 
-        let cpu_values: Vec<f64> = cpu_vec.drain(2..).map(|x| x.parse().unwrap()).collect();
+        let cpu_values: Vec<f64> = cpu_vec
+            .drain(2..)
+            .map(| x | x.parse().unwrap())
+            .collect();
 
         let cpu_usage = self.calculate_current_cpu_usage(&cpu_values);
         Ok(cpu_usage)
@@ -73,8 +80,8 @@ impl CPUUsage {
             self.prev_usage = current_usage;
             self.prev_time = current_time;
         }
-        let current_cpu =
-            (current_usage - self.prev_usage) * 100.0 / (current_time - self.prev_time);
+        let current_cpu = (current_usage - self.prev_usage) * 100.0 / 
+            (current_time - self.prev_time);
         self.prev_usage = current_usage;
         self.prev_time = current_time;
         self.current_cpu = current_cpu;
@@ -89,16 +96,20 @@ pub struct MemInfo {
 }
 
 impl MemInfo {
+
     pub fn new() -> MemInfo {
         let usage = vec![];
         let current_mem = 0.0;
-        MemInfo { usage, current_mem }
+        MemInfo {
+            usage,
+            current_mem,
+        }
     }
 
     pub fn clear_usage(&mut self) {
         if self.usage.len() >= 300 {
             self.usage.clear();
-        }
+        } 
     }
 
     pub fn get_usage(&self) -> &Vec<(f64, f64)> {
@@ -109,40 +120,37 @@ impl MemInfo {
         self.current_mem
     }
 
-    pub fn extract_kb_info(&self, line: Vec<&str>) -> f64 {
-        let line_vec: Vec<&str> = line[0].split_whitespace().collect();
-        let kb: f64 = line_vec[1].parse().unwrap();
-        kb
-    }
-
+    
     pub fn get_mem_info(&mut self) -> Result<f64, io::Error> {
         let mem_file_path = Path::new("/proc/meminfo");
         let mem_file = fs::read_to_string(mem_file_path)?;
-        let mem_total_line: Vec<&str> = mem_file
-            .lines()
+        let mem_total_line: Vec<&str> = mem_file.lines()
             .filter(|line| line.contains("MemTotal"))
             .collect();
-        let mem_free_line: Vec<&str> = mem_file
-            .lines()
+       let mem_free_line: Vec<&str> = mem_file.lines()
             .filter(|line| line.contains("MemFree"))
             .collect();
-        let mem_total = self.extract_kb_info(mem_total_line);
-        let mem_free = self.extract_kb_info(mem_free_line);
+        let mem_total = extract_kb_info(mem_total_line);
+        let mem_free = extract_kb_info(mem_free_line);
         let percentage_used = mem_free / mem_total * 100.0;
         self.current_mem = percentage_used;
         Ok(percentage_used)
+
     }
 
-    pub fn add_mem_data(&mut self) -> io::Result<()> {
+    pub fn add_mem_data(&mut self) -> io::Result<()>{
         self.clear_usage();
         let new_mem_data = self.get_mem_info()?;
         let time_to_add = match self.usage.last() {
-            Some(x) => x.0 + 1.0,
-            None => 1.0,
+             Some(x) =>  x.0 + 1.0,
+             None => 1.0
         };
         self.usage.push((time_to_add, new_mem_data));
         Ok(())
     }
+
+
+
 }
 
 #[derive(Debug, Clone)]
@@ -155,10 +163,12 @@ pub struct Process {
     pub total_time: f64,
     pub mem_percent: f64,
     pub cpu_percent: f64,
+    pub rss: f64,
 }
 
 impl Process {
-    pub fn new(pid: i32, state: String, process_name: String, utime: f64, stime: f64) -> Process {
+
+    pub fn new(pid: i32, state: String,  process_name: String, utime: f64, stime: f64, rss: f64) -> Process {
         let total_time = utime + stime;
         let mem_percent = 0.0;
         let cpu_percent = 0.0;
@@ -171,31 +181,58 @@ impl Process {
             total_time,
             mem_percent,
             cpu_percent,
+            rss,
         }
     }
 
     pub fn set_cpu_percent(&mut self, percent: f64) {
         self.cpu_percent = percent;
     }
+
+    pub fn set_utime(&mut self, utime: f64) {
+        self.utime = utime;
+    }
+
+    pub fn set_stime(&mut self, stime: f64) {
+        self.stime = stime;
+    }
+
+    pub fn set_total_time(&mut self, total_time: f64) {
+        self.total_time = total_time;
+    }
+
+    pub fn set_mem_percent(&mut self, mem_percent: f64) {
+        self.mem_percent = mem_percent;
+    }
+
+    pub fn set_rss(&mut self, rss: f64) {
+        self.rss = rss;
+    }
+
 }
 
 #[derive(Debug)]
 pub struct ProcessInfo {
-    processes: Vec<Process>,
+    processes: Vec<Process>, 
     cpu_time_diff: f64,
     current_cpu_time: f64,
+    total_mem: f64,
 }
 
+
 impl ProcessInfo {
-    pub fn new() -> ProcessInfo {
+    
+    pub fn new() -> Result<ProcessInfo, io::Error> {
         let processes = vec![];
         let cpu_time_diff = 0.0;
         let current_cpu_time = 0.0;
-        ProcessInfo {
+        let total_mem = get_total_mem_info()?;
+       Ok(ProcessInfo {
             processes,
             cpu_time_diff,
             current_cpu_time,
-        }
+            total_mem,
+        })
     }
 
     pub fn get_processes(&self) -> &Vec<Process> {
@@ -207,7 +244,7 @@ impl ProcessInfo {
             .sort_by(|a, b| b.cpu_percent.partial_cmp(&a.cpu_percent).unwrap());
     }
 
-    pub fn update(&mut self, proc_path: &Path) -> Result<(), io::Error> {
+    pub fn update(&mut self, proc_path: &Path) -> Result<(), io::Error>{
         self.update_cpu_diff()?;
         self.read_dirs(&proc_path)?;
         self.sort_by_cpu();
@@ -223,16 +260,28 @@ impl ProcessInfo {
             path = entry.path();
             if path.is_dir() && digits_only.is_match(path.file_name().unwrap().to_str().unwrap()) {
                 dirs.push(path);
-            } else if path.is_file()
-                && path.file_name().unwrap() == "stat"
-                && proc_path.to_str().unwrap() != "/proc/"
-            {
+            }
+            else if path.is_file() && path.file_name().unwrap() == "stat" 
+                && proc_path.to_str().unwrap() != "/proc/" {
                 let contents = match fs::read_to_string(&path) {
                     Ok(f) => f,
-                    Err(e) => e.to_string(),
+                    Err(e) => {
+                        e.to_string()
+                    },
                 };
-                self.get_mem_data(&contents);
+                self.get_cpu_data(&contents);
             }
+            else if path.is_file() && path.file_name().unwrap() == "status" 
+                && proc_path.to_str().unwrap() != "/proc/" {
+                let contents = match fs::read_to_string(&path) {
+                    Ok(f) => f,
+                    Err(e) => {
+                        e.to_string()
+                    },
+                };
+                self.get_proccess_mem(&contents);
+            }
+
         }
         for dir in dirs.iter() {
             self.read_dirs(&dir);
@@ -240,16 +289,82 @@ impl ProcessInfo {
         Ok(())
     }
 
-    pub fn get_mem_data(&mut self, contents: &str) -> Result<(), io::Error> {
-        let contents_array: Vec<&str> = contents.split_whitespace().collect();
+    pub fn get_proccess_mem(&mut self, contents: &str) -> Result<(), io::Error> {
+        let mut rss = 0.0;
+        let mem_total_line: Vec<&str> = contents.lines()
+            .filter(|line| line.contains("VmRSS"))
+            .collect();
+        if !mem_total_line.is_empty(){
+            let mem_total_vec: Vec<&str> = mem_total_line[0]
+                .split_whitespace()
+                .collect();
+            rss = mem_total_vec[1].parse().unwrap();
+        }
+                
+        let pid_vec = get_line_from_file(&contents, "Pid");
+        let state_vec = get_line_from_file(&contents, "State");
+        let name_vec = get_line_from_file(&contents, "Name");
+        let pid: i32 = pid_vec[1].parse().unwrap();
+        let process_name = String::from(name_vec[0]);
+        let state = String::from(state_vec[0]);
+        let mem_percent = rss / self.total_mem * 100.0;
+        let utime = 0.0;
+        let stime = 0.0;
+        let total_time = 0.0;
+        let cpu_percent = 0.0;
+        let mut new_process = Process::new(
+            pid,
+            process_name,
+            state,
+            utime,
+            stime,
+            rss,
+        );
+        new_process.set_mem_percent(mem_percent);
+        self.add_mem_info_to_processes(new_process);
+        Ok(())
+    }
+
+    pub fn add_mem_info_to_processes(&mut self, mut process: Process) -> Result<(), io::Error> {
+        let found_process = self.processes.iter()
+            .find(|p| p.pid == process.pid);
+        match found_process {
+            Some(p) => {
+                process.set_stime(p.stime);
+                process.set_utime(p.utime);
+                process.set_total_time(p.total_time);
+                process.set_cpu_percent(p.cpu_percent);
+                let filtered_processes: Vec<Process> = self.processes.iter().cloned()
+                    .filter(| x| x.pid != process.pid)
+                    .collect();
+                self.processes = filtered_processes;
+                self.processes.push(process);
+            },
+            None => self.processes.push(process),
+        };
+        Ok(())
+    }
+
+    pub fn get_cpu_data(&mut self, contents: &str) -> Result<(), io::Error> {
+        let contents_array: Vec<&str> = contents
+            .split_whitespace()
+            .collect();
         let pid: i32 = contents_array[0].parse().unwrap();
         let process_name: String = contents_array[2].replace("(", "").replace(")", "");
         let state: String = contents_array[1].replace("(", "").replace(")", "");
         let utime: f64 = contents_array[13].parse().unwrap();
         let stime: f64 = contents_array[14].parse().unwrap();
+        let rss = 0.0;
         let total_time = utime + stime;
-        let mem_data = Process::new(pid, process_name, state, utime, stime);
-        self.add_to_processes(mem_data);
+        let mem_data = Process::new(
+            pid,
+            process_name,
+            state,
+            utime,
+            stime,
+            rss,
+        );
+        self.add_cpu_info_to_processes(mem_data);
         Ok(())
     }
 
@@ -260,34 +375,35 @@ impl ProcessInfo {
         Ok(())
     }
 
-    pub fn add_to_processes(&mut self, mut process: Process) -> Result<(), io::Error> {
-        let found_process = self.processes.iter().find(|p| p.pid == process.pid);
+
+    pub fn add_cpu_info_to_processes(&mut self, mut process: Process) -> Result<(), io::Error> {
+        let found_process = self.processes.iter()
+            .find(|p| p.pid == process.pid);
         match found_process {
             Some(p) => {
                 let utime_percent = 100.0 * (process.utime - p.utime) / self.cpu_time_diff;
                 let stime_percent = 100.0 * (process.stime - p.stime) / self.cpu_time_diff;
                 let percent = 100.0 * (process.total_time - p.total_time) / self.cpu_time_diff;
                 process.set_cpu_percent(percent);
-                let filtered_processes: Vec<Process> = self
-                    .processes
-                    .iter()
-                    .cloned()
-                    .filter(|x| x.pid != process.pid)
+                process.set_rss(p.rss);
+                process.set_mem_percent(p.mem_percent);
+                let filtered_processes: Vec<Process> = self.processes.iter().cloned()
+                    .filter(| x| x.pid != process.pid)
                     .collect();
                 self.processes = filtered_processes;
                 self.processes.push(process);
-            }
+            },
             None => self.processes.push(process),
         };
         Ok(())
     }
 
-    pub fn get_cpu_info(&mut self) -> Result<f64, io::Error> {
+    pub fn get_cpu_info(&mut self) -> Result<f64, io::Error>{
         let mut cpu_time = 0.0;
         let cpu_vec = create_cpu_vector()?;
         for val in cpu_vec.iter() {
             cpu_time += val;
-        }
+        } 
         Ok(cpu_time)
     }
 }
@@ -298,9 +414,44 @@ pub fn create_cpu_vector() -> Result<Vec<f64>, io::Error> {
     let cpu_info = match cpu_file.lines().next() {
         Some(i) => i,
         None => "File Error",
-    };
-    let mut cpu_vec: Vec<&str> = cpu_info.split(' ').collect();
+     };
+    let mut cpu_vec: Vec<&str> = cpu_info
+        .split(' ')
+        .collect();
 
-    let cpu_values: Vec<f64> = cpu_vec.drain(2..).map(|x| x.parse().unwrap()).collect();
+    let cpu_values: Vec<f64> = cpu_vec
+        .drain(2..)
+        .map(| x | x.parse().unwrap())
+        .collect();
     Ok(cpu_values)
+}
+
+pub fn extract_kb_info(line: Vec<&str>) -> f64 {
+        let line_vec: Vec<&str> = line[0]
+            .split_whitespace()
+            .collect();
+        let kb: f64 = line_vec[1]
+            .parse()
+            .unwrap();
+        kb    
+}
+
+pub fn get_total_mem_info() -> Result<f64, io::Error> {
+        let mem_file_path = Path::new("/proc/meminfo");
+        let mem_file = fs::read_to_string(mem_file_path)?;
+        let mem_total_line: Vec<&str> = mem_file.lines()
+            .filter(|line| line.contains("MemTotal"))
+            .collect();
+        let total_mem = extract_kb_info(mem_total_line);
+        Ok(total_mem)
+ }
+
+pub fn get_line_from_file<'a>(file: &'a str, pattern: &str) -> Vec<&'a str> {
+    let found_line: Vec<&str> = file.lines()
+            .filter(|line| line.contains(pattern))
+            .collect();
+    let found_vec: Vec<&str> = found_line[0]
+            .split_whitespace()
+            .collect();
+    found_vec
 }
--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,28 @@
 mod util;
 
-use std::io;
-use std::path::Path;
-use std::process;
-use std::{thread, time};
 use system_info::{CPUUsage, MemInfo, ProcessInfo};
+use std::process;
+use std::io;
 use termion::event::Key;
 use termion::input::MouseTerminal;
 use termion::raw::IntoRawMode;
 use termion::screen::AlternateScreen;
 use tui::backend::TermionBackend;
-use tui::layout::{Constraint, Layout};
 use tui::style::{Color, Modifier, Style};
-use tui::widgets::{Axis, Block, Borders, Chart, Dataset, Marker, Row, Table, Widget};
+use tui::layout::{Constraint, Layout};
+use tui::widgets::{Axis, Row, Table,  Block, Borders, Chart, Dataset, Marker, Widget};
 use tui::Terminal;
+use std::path::Path;
+use std::{thread, time};
 
 use util::event::{Event, Events};
 
+
 fn main() -> Result<(), failure::Error> {
-    let mut process_info = ProcessInfo::new();
+    let mut process_info = ProcessInfo::new()?;
     let proc_path = Path::new("/proc/");
-    process_info.update(&proc_path)?;
-    let second = time::Duration::from_millis(1000);
+    process_info.update(&proc_path)?; 
+    let second = time::Duration::from_millis(1000); 
     thread::sleep(second);
     process_info.update(&proc_path)?;
     let events = Events::new();
@@ -35,80 +36,69 @@ fn main() -> Result<(), failure::Error> {
     terminal.hide_cursor()?;
     loop {
         terminal.draw(|mut f| {
-            let chunks = Layout::default()
-                .constraints([Constraint::Length(20), Constraint::Min(0)].as_ref())
-                .split(f.size());
-            let size = f.size();
-            Chart::default()
-                .block(
-                    Block::default()
-                        .title("CPU/Memory")
-                        .title_style(Style::default().fg(Color::Cyan).modifier(Modifier::BOLD))
-                        .borders(Borders::ALL),
-                )
-                .x_axis(
-                    Axis::default()
-                        .title("Seconds")
-                        .style(Style::default().fg(Color::Gray))
-                        .labels_style(Style::default().modifier(Modifier::ITALIC))
-                        .bounds([0.0, 300.0])
-                        .labels(&["0", "50", "100", "150", "200", "250", "300"]),
-                )
-                .y_axis(
-                    Axis::default()
-                        .title("%")
-                        .style(Style::default().fg(Color::Gray))
-                        .labels_style(Style::default().modifier(Modifier::ITALIC))
-                        .bounds([0.0, 100.0])
-                        .labels(&["0", "20", "40", "60", "80", "100"]),
-                )
-                .datasets(&[
-                    Dataset::default()
-                        .name(&format!("CPU {:.2}%", cpu_usage.get_current_cpu()))
-                        .marker(Marker::Dot)
-                        .style(Style::default().fg(Color::Cyan))
-                        .data(&cpu_usage.get_usage()[..]),
-                    Dataset::default()
-                        .name(&format!("Memory {:.2}%", mem_info.get_current_mem()))
-                        .marker(Marker::Dot)
-                        .style(Style::default().fg(Color::Magenta))
-                        .data(&mem_info.get_usage()[..]),
-                ])
-                .render(&mut f, chunks[0]);
+        let chunks = Layout::default()
+            .constraints([Constraint::Length(20), Constraint::Min(0)].as_ref())
+            .split(f.size());
+        let size = f.size();
+        Chart::default()
+            .block(
+              Block::default()
+                      .title("CPU/Memory")
+                      .title_style(Style::default().fg(Color::Cyan).modifier(Modifier::BOLD))
+                      .borders(Borders::ALL),
+              )
+              .x_axis(
+                  Axis::default()
+                      .title("Seconds")
+                      .style(Style::default().fg(Color::Gray))
+                      .labels_style(Style::default().modifier(Modifier::ITALIC))
+                      .bounds([0.0, 300.0])
+                      .labels(&["0", "50", "100", "150", "200", "250", "300"]),
+              )
+              .y_axis(
+                  Axis::default()
+                      .title("%")
+                      .style(Style::default().fg(Color::Gray))
+                      .labels_style(Style::default().modifier(Modifier::ITALIC))
+                      .bounds([0.0, 100.0])
+                      .labels(&["0", "20", "40", "60", "80", "100"]),
+              )
+              .datasets(&[ 
+                  Dataset::default()
+                      .name(&format!("CPU {:.2}%", cpu_usage.get_current_cpu()))
+                      .marker(Marker::Dot)
+                      .style(Style::default().fg(Color::Cyan))
+                      .data(&cpu_usage.get_usage()[..]),
+                  Dataset::default()
+                      .name(&format!("Memory {:.2}%", mem_info.get_current_mem()))
+                      .marker(Marker::Dot)
+                      .style(Style::default().fg(Color::Magenta))
+                      .data(&mem_info.get_usage()[..]),
+
+              ])
+              .render(&mut f, chunks[0]);
 
             let selected_style = Style::default().fg(Color::Yellow).modifier(Modifier::BOLD);
             let normal_style = Style::default().fg(Color::White);
-            let header = [
-                "PID",
-                "Process Name",
-                "State",
-                "UTime",
-                "STime",
-                "Total Time",
-                "Memory %",
-                "CPU %",
-            ];
-            let rows = process_info
-                .get_processes()
-                .iter()
-                .enumerate()
-                .map(|(i, process)| {
-                    let process_vec: Vec<String> = vec![
-                        process.pid.to_string(),
-                        process.process_name.clone(),
-                        process.state.clone(),
-                        process.utime.to_string(),
-                        process.stime.to_string(),
-                        process.total_time.to_string(),
-                        format!("{:.2}", process.mem_percent),
-                        format!("{:.2}", process.cpu_percent),
+            let header = ["PID", "Process Name","State", "UTime", "STime", "Total Time", "RSS", "Memory %",  "CPU %"];
+            let rows = process_info.get_processes().iter().enumerate().map(|(i, process)| {
+                let process_vec: Vec<String> = vec![
+                    process.pid.to_string(),
+                    process.process_name.clone(),
+                    process.state.clone(),
+                    process.utime.to_string(),
+                    process.stime.to_string(),
+                    process.total_time.to_string(),
+                    process.rss.to_string(),
+                    format!("{:.2}", process.mem_percent),
+                    format!("{:.2}", process.cpu_percent),
                     ];
-                    Row::StyledData(process_vec.into_iter(), normal_style)
-                    // if i == app.selected {
-                    //} else {
-                    // Row::StyledData(item.into_iter(), normal_style)
-                    // }
-                });
+                Row::StyledData(process_vec.into_iter(), normal_style) 
+                // if i == app.selected {
+                //} else {
+                // Row::StyledData(item.into_iter(), normal_style)
+                // }
+            });
             // println!("{:?}", rows);
             let rects = Layout::default()
                 .constraints([Constraint::Percentage(100)].as_ref())
@@ -116,31 +106,43 @@ fn main() -> Result<(), failure::Error> {
                 .split(f.size());
             Table::new(header.into_iter(), rows)
                 .block(Block::default().borders(Borders::ALL).title("Processes"))
-                .widths(&[30, 30, 20, 20, 20, 20, 20, 20])
-                .render(&mut f, chunks[1]);
+                .widths(&[
+                    20,
+                    20,
+                    20,
+                    20,
+                    20,
+                    20,
+                    20,
+                    20,
+                    20,
+                ])
+                .render(&mut f, chunks[1]); 
         })?;
-        match events.next()? {
-            Event::Input(input) => {
-                if input == Key::Char('q') {
-                    println!("quit");
-                    break;
-                }
-            }
-            Event::Tick => {
-                if let Err(e) = cpu_usage.add_cpu_data() {
-                    eprintln!("Application error: {}", e);
-                    process::exit(1);
-                }
-                if let Err(e) = mem_info.add_mem_data() {
-                    eprintln!("Application error: {}", e);
-                    process::exit(1);
-                }
-                if let Err(e) = process_info.update(&proc_path) {
-                    eprintln!("Application error: {}", e);
-                    process::exit(1);
-                }
-            }
-        }
+      match events.next()? {
+          Event::Input(input) => {
+              if input == Key::Char('q') {
+                  println!("quit");
+                  break;
+              }
+
+          }
+          Event::Tick => {
+              if let Err(e) = cpu_usage.add_cpu_data() {
+                  eprintln!("Application error: {}", e);
+                  process::exit(1);
+              }
+              if let Err(e) = mem_info.add_mem_data() {
+                  eprintln!("Application error: {}", e);
+                  process::exit(1);
+              }
+              if let Err(e) = process_info.update(&proc_path) {
+                  eprintln!("Application error: {}", e);
+                  process::exit(1);
+              }   
+          }
+
+      }
     }
     Ok(())
 }
